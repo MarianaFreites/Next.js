4bfe3986dcf2a64222505f7f308f40ac7ef94a7c
MarianaFreites <marianafreitessantana@iresm.edu.ar>
2024-11-14 00:03:19 -0300

    Primer commit

---
+++ nextjs-dashboard/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from 'next';
+// next.config.js
+module.exports = {
+  experimental: {
+    appDir: true,
+  },
+};
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
